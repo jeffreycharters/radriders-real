2149c67497cfb5519b14b6497cd2752bc07427c6
dietBlue <jeff.charters@gmail.com>
2019-12-05 19:57:42 -0500

    Added user registration and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
@@ -12,6 +14,8 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
+    login.login_view = 'users_bp.login'
 
     with app.app_context():
         from app import routes
--- app/users/routes.py
+++ app/users/routes.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, redirect, flash, url_for
+from flask import Blueprint, render_template, redirect, flash, url_for, request
 from flask import current_app as app
-from app.users.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app import db
+from app.users.forms import LoginForm, RegistrationForm
 from app.users.models import User
+from werkzeug.urls import url_parse
 
 users_bp = Blueprint('users_bp', __name__,
                      template_folder='templates',
@@ -10,12 +13,44 @@ users_bp = Blueprint('users_bp', __name__,
 
 @users_bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, \
-              remember me={form.remember_me.data}')
-        redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('users_bp.login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@users_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@users_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print('Validated')
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('users_bp.login'))
+    print('No validated')
+    return render_template('register.html', title='Get Registered', form=form)
 
 
 @users_bp.route('/user/<username>', methods=['GET'])

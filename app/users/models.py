1696f32ee81cd0e609b863b8d4e00695ec9f003b
dietBlue <jeff.charters@gmail.com>
2019-12-08 16:45:46 -0500

    Use include for statuses, add active to user

--- app/users/models.py
+++ app/users/models.py
@@ -9,16 +9,18 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     admin = db.Column(db.Boolean, default=False)
-    statuses = db.relationship('Status', backref='author', lazy='dynamic')   
+    active = db.Column(db.Boolean, default=True)
+    statuses = db.relationship('Status', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f'<user { self.username }>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
 
 @login.user_loader
 def load_user(id):

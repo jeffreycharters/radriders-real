b3ede367cd8b9ffb1bd4660c9cd12d99570dba1a
dietBlue <jeff.charters@gmail.com>
2019-12-15 14:32:17 -0500

    Add active and report columns to status model

--- app/status/models.py
+++ app/status/models.py
@@ -8,6 +8,18 @@ class Status(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     trail_system = db.Column(db.Integer, db.ForeignKey('trails.id'))
+    active = db.Column(db.Boolean, default=True)
+    reported = db.Column(db.Integer, default=0)
+
+    def activate(self):
+        if not self.active:
+            self.active = True
+            db.session.commit()
+
+    def deactivate(self):
+        if self.active:
+            self.active = False
+            db.session.commit()
 
     def __repr__(self):
         return f'<Status {self.author, self.trails, self.body }>'

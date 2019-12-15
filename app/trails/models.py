f465f2ea2782e840c5d07efbc0342777c5d37cdf
dietBlue <jeff.charters@gmail.com>
2019-12-15 14:34:21 -0500

    Add trails active column

--- app/trails/models.py
+++ app/trails/models.py
@@ -7,7 +7,18 @@ class Trails(db.Model):
     city = db.Column(db.String(64))
     province = db.Column(db.String(2))
     approved = db.Column(db.Boolean, default=False)
+    active = db.Column(db.Boolean, default=True)
     statuses = db.relationship('Status', backref='trails', lazy='dynamic')
 
     def __repr__(self):
         return f'<Trail system { self.name }>'
+
+    def activate(self):
+        if not self.active:
+            self.active = True
+            db.session.commit()
+
+    def deactivate(self):
+        if self.active:
+            self.active = False
+            db.session.commit()

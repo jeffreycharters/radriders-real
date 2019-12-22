8ab6533b356f4160f4d64587db768cdbda57a671
dietBlue <jeff.charters@gmail.com>
2019-12-22 16:04:20 -0500

    Add admin edit trails

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_required
 from app import db
 from app.status.models import Status
 from app.trails.models import Trails
+from app.trails.forms import TrailAddForm
 from app.users.models import User
 from werkzeug.urls import url_parse
 
@@ -29,9 +30,11 @@ def activate(table, id):
         return redirect(url_for('trails_bp.index'))
     if table == 'user':
         item = User.query.filter_by(id=id).first()
-        if item is None:
-            flash('Couldn\'t find item ' + id)
-            return redirect(url_for('admin_bp.list_user'))
+    elif table == 'trails':
+        item = Trails.query.filter_by(id=id).first()
+    if item is None:
+        flash('Couldn\'t find item ' + id)
+        return redirect(url_for('admin_bp.list_'+table))
     item.activate()
     flash('Activated ' + table + ': ' + str(item.id))
     return redirect(url_for('admin_bp.list_'+table))
@@ -44,12 +47,37 @@ def deactivate(table, id):
         return redirect(url_for('trails_bp.index'))
     if table == 'user':
         item = User.query.filter_by(id=id).first()
-        if item is None:
-            flash('Couldn\'t find item ' + id)
-            return redirect(url_for('admin_bp.list_user'))
+    elif table == 'trails':
+        item = Trails.query.filter_by(id=id).first()
+    if item is None:
+        flash('Couldn\'t find item ' + id)
+        return redirect(url_for('admin_bp.list_'+table))
     item.deactivate()
     flash('Deactivated ' + table + ': ' + str(item.id))
     return redirect(url_for('admin_bp.list_'+table))
+
+
+@admin_bp.route('/edit_trails/<trails_id>', methods=['GET', 'POST'])
+def edit_trails(trails_id):
+    if not current_user.admin:
+        return redirect(url_for('trails_bp.index'))
+    trails = Trails.query.filter_by(id=trails_id).first_or_404()
+    form = TrailAddForm()
+    if form.validate_on_submit():
+        trails.name = form.trail_name.data
+        trails.city = form.city.data
+        trails.province = form.province.data
+        trails.trailforks = form.trailforks.data
+        db.session.commit()
+        return redirect(url_for('admin_bp.list_trails'))
+    elif request.method == 'GET':
+        form.trail_name.data = trails.name
+        form.city.data = trails.city
+        form.province.data = trails.province
+        form.trailforks.data = trails.trailforks
+        form.submit.label.text = 'Update'
+    return render_template('edit_trails.html', title='Edit Trails',
+                           form=form, trails=trails)
 
 
 @admin_bp.route('/list_status')
@@ -66,8 +94,10 @@ def list_status():
 def list_trails():
     if not current_user.admin:
         return redirect(url_for('trails_bp.index'))
+    titles = Trails.__table__.columns.keys()
     items = Trails.query.all()
-    return render_template('list_trails.html', title='Trails List', items=items)
+    return render_template('list_trails.html', title='Trails List',
+                           items=items, titles=titles)
 
 
 @admin_bp.route('/list_user')
